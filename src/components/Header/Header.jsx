cf7ffedf4fb56a8c244850fbf59ae458acf64aab
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-10-07 14:17:05 -0300

    Fix: li to anchor tag

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, ContainerLogo, Logo, Menu } from './Style'
+import { Container, ContainerLogo, Logo, Menu, MenuOptions } from './Style'
 import LogoCodetech from '../../assets/Logo-Codetech.jpeg'
 
 function Header() {
@@ -10,9 +10,21 @@ function Header() {
       </ContainerLogo>
       <section>
         <Menu>
-          <li>Quem Somos</li>
-          <li>Produtos</li>
-          <li>Contato</li>
+          <MenuOptions>
+            <a href="">
+              Quem Somos
+            </a>
+          </MenuOptions>
+          <MenuOptions>
+            <a href="">
+              Produtos
+            </a>
+          </MenuOptions>
+          <MenuOptions>
+            <a href="">
+              Contato
+            </a>
+          </MenuOptions>
         </Menu>
       </section>
     </Container>
