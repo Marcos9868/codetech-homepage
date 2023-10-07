3fcc22407f63ac616441b73dad004dd0831bb914
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-10-07 14:16:38 -0300

    Feat: anchor styles on Social Media and change bg

--- src/components/Footer/Style.js
+++ src/components/Footer/Style.js
@@ -4,8 +4,8 @@ export const Container = styled.div`
   display: flex;
   width: 100%;
   height: 8.5rem;
-  background-color: #f7f8f5;
-  border: 2px solid red;
+  background-color: #121212;
+  color: #f5f5f5;
   bottom: 0;
   flex-direction: column;
   justify-content: center;
@@ -14,16 +14,25 @@ export const Container = styled.div`
 
 export const ContainerSocialMedia = styled.section`
   width: 100%;
-  height: 50%;
+  height: 40%;
   justify-self: flex-start;
 `;
 
 export const SocialMedia = styled.ul`
   display: flex;
   width: 100%;
-  height: 100%;
+  height: 80%;
   list-style: none;
   justify-content: center;
   align-items: center;
   gap: 3.5rem;
+
+  a {
+    gap: 0.4rem;
+    list-style: none;
+    text-decoration: none;
+    outline: none;
+    color: #f5f5f5;
+    cursor: pointer;
+  }
 `;
